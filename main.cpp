a8846e23808a67b832de9b08ff41e6aa42aed87c
Silas Maughan <silas.maughan@gmail.com>
2024-01-16 09:59:21 +0100

    Allow resizing

--- main.cpp
+++ main.cpp
@@ -1,47 +1,33 @@
 #include <iostream>
 
-// GLFW
 #define GLFW_DLL
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <GLUT/glut.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 const GLint width = 800;
 const GLint height = 600;
 
-void processInput(GLFWwindow *window)
-{
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
-int main(void)
+int main()
 {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // safe on mac
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
   GLFWwindow *window = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr);
-
-  int screenWidth;
-  int screenHeight;
-
-  glfwGetFramebufferSize(window, &screenWidth, &screenHeight); // safe on mac
-
-  if (nullptr == window)
+  if (window == nullptr)
   {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-
     return -1;
   }
-
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
@@ -49,28 +35,30 @@ int main(void)
     return -1;
   }
 
-  // init vertices
-
-  // init viewport
-  glViewport(0, 0, screenWidth, screenHeight);
-
-  // print supported webgl version
-  std::cout << (glGetString(GL_VERSION));
-
+  // Render loop
   while (!glfwWindowShouldClose(window))
   {
-
     processInput(window);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(window);
-
     glfwPollEvents();
   }
 
   glfwTerminate();
-
   return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: window size changed, callback executes
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
 }
