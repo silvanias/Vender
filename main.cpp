970898e4b984557b7ae0b47bd8af7913f95681d5
Silas Maughan <silas.maughan@gmail.com>
2024-01-20 18:47:19 +0100

    fragment interpolated square

--- main.cpp
+++ main.cpp
@@ -13,15 +13,20 @@ const GLint height = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
+                                  
                                  "void main()\n"
                                  "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "   ourColor = aColor;\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
+                                   "in vec3 ourColor;\n" 
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "   FragColor = vec4(ourColor, 1.0f);\n"
                                    "}\n\0";
 
 int main()
@@ -94,14 +99,14 @@ int main()
   glDeleteShader(fragmentShader);
 
   // Set up vertices
-  std::array<float, 12> vertices = {
-      0.5f, 0.5f, 0.0f,   // top right
-      0.5f, -0.5f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f, // bottom left
-      -0.5f, 0.5f, 0.0f   // top left
+  std::array<float, 24> vertices = {
+      // pos            // col
+      0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // top right
+      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, // bottom left
+      -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // top left
   };
   std::array<unsigned int, 6> indices = {
-      // note that we start from 0!
       0, 1, 3, // first triangle
       1, 2, 3  // second triangle
   };
@@ -121,8 +126,11 @@ int main()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid *)nullptr);
   glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // Render loop
   while (!glfwWindowShouldClose(window))
@@ -131,9 +139,10 @@ int main()
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    // Draw triangle
+    
     glUseProgram(shaderProgram);
+    // Draw triangle
+    
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
