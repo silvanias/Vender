acdc1f84beaa9a43a80730a2571da5b31c028e1d
Silas Maughan <silas.maughan@gmail.com>
2024-02-04 14:57:11 +0100

    texturing

--- BuildCommands.sh
+++ BuildCommands.sh
@@ -6,8 +6,5 @@ cd build
 # Build using CMake
 cmake --build .
 
-# Go back to the parent directory
-cd ..
-
 # Execute the 'vender' command in the 'build' directory
-./build/vender
+./vender
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <GLUT/glut.h>
 
 #include "shader.h"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -38,16 +39,17 @@ int main()
   }
 
   // Shader instantiation
-  Shader aShader("shaders/shader.vs", "shaders/shader.fs");
+  Shader aShader("../shaders/shader.vs", "../shaders/shader.fs");
 
   // Set up vertices
-  std::array<float, 24> vertices = {
-      // pos            // col
-      0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right
-      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, // bottom left
-      -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // top left
+  std::array<float, 32> vertices = {
+      // positions          // colors           // texture coords
+      0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
   };
+
   std::array<unsigned int, 6> indices = {
       0, 1, 3, // first triangle
       1, 2, 3  // second triangle
@@ -68,11 +70,40 @@ int main()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid *)nullptr);
+  // Position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid *)nullptr);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid *)(3 * sizeof(float)));
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+
+  // Texture attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // Texturing
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load("../resources/container.jpg", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
 
   // Render loop
   while (!glfwWindowShouldClose(window))
@@ -85,6 +116,7 @@ int main()
     aShader.use();
 
     // Draw triangle
+    glBindTexture(GL_TEXTURE_2D, texture);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
