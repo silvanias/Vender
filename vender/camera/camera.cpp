500fdda946ae3630493676b43f5ffe54eed62be8
Silas Maughan <silas.maughan@gmail.com>
2024-04-11 22:36:44 +0200

    remove implementation from camera.h

---
+++ vender/camera/camera.cpp
@@ -0,0 +1,49 @@
+#include "camera.h"
+
+glm::mat4 Camera::calculateView() const
+{
+    return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+}
+
+void Camera::processKeyboard(Direction direction, float deltaTime)
+{
+    float cameraSpeed = speed * deltaTime;
+    if (direction == Direction::UP)
+        cameraPos += cameraSpeed * cameraFront;
+    if (direction == Direction::DOWN)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (direction == Direction::LEFT)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (direction == Direction::RIGHT)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void Camera::processMouse(float xoffset, float yoffset)
+{
+
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
+void Camera::processZoom(float yoffset)
+{
+    fov -= yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 90.0f)
+        fov = 90.0f;
+}
--- vender/camera/camera.h
+++ vender/camera/camera.h
@@ -4,7 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-enum Direction
+enum struct Direction
 {
     LEFT,
     RIGHT,
@@ -21,65 +21,14 @@ public:
     float speed = 2.5f;
     float sensitivity = 0.1f;
 
-    glm::vec3 cameraPos;
-    glm::vec3 cameraFront;
-    glm::vec3 cameraUp;
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-    Camera(
-        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f),
-        glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f))
-        : cameraPos(cameraPos), cameraFront(cameraFront), cameraUp(cameraUp)
-    {
-    }
-
-    glm::mat4 calculateView() const
-    {
-        return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-    }
-
-    void processKeyboard(Direction direction, float deltaTime)
-    {
-        float cameraSpeed = speed * deltaTime;
-        if (direction == UP)
-            cameraPos += cameraSpeed * cameraFront;
-        if (direction == DOWN)
-            cameraPos -= cameraSpeed * cameraFront;
-        if (direction == LEFT)
-            cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-        if (direction == RIGHT)
-            cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    }
-
-    void processMouse(float xoffset, float yoffset)
-    {
-
-        xoffset *= sensitivity;
-        yoffset *= sensitivity;
-
-        yaw += xoffset;
-        pitch += yoffset;
-
-        if (pitch > 89.0f)
-            pitch = 89.0f;
-        if (pitch < -89.0f)
-            pitch = -89.0f;
-
-        glm::vec3 direction;
-        direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-        direction.y = sin(glm::radians(pitch));
-        direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-        cameraFront = glm::normalize(direction);
-    }
-
-    void processZoom(float yoffset)
-    {
-        fov -= yoffset;
-        if (fov < 1.0f)
-            fov = 1.0f;
-        if (fov > 90.0f)
-            fov = 90.0f;
-    }
+    glm::mat4 calculateView() const;
+    void processKeyboard(Direction direction, float deltaTime);
+    void processMouse(float xoffset, float yoffset);
+    void processZoom(float yoffset);
 };
 
 #endif
